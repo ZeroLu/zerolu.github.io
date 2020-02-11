845868829c021d2dbbeee4f62613ddae5ca66ff8
ZeroLu <iamzerolu@gmail.com>
2020-02-11 16:57:12 +0800

    update watchar videos

--- javascripts/main.js
+++ javascripts/main.js
@@ -23,6 +23,23 @@ var bodyStyler = {
     };
 var pageData = {
 };
+function smoothScroll(el, to, duration) {
+    if (duration < 0) {
+        return;
+    }
+    var difference = to - $(window).scrollTop();
+    var perTick = difference / duration * 10;
+    this.scrollToTimerCache = setTimeout(function() {
+        if (!isNaN(parseInt(perTick, 10))) {
+            window.scrollTo(0, $(window).scrollTop() + perTick);
+            smoothScroll(el, to, duration - 10);
+        }
+    }.bind(this), 10);
+}
+function scrollToContact() {
+    // body...
+    smoothScroll($(window), 3000, 500);
+}
 var patentListOpen = true;
 function toggleList(argument) {
     var patentList = $("ul#patentList")
